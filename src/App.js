80332398bd174cd9685584e6d23b24a69b1d13d3
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-03 02:09:34 +0600

    favicon added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
 import AddItems from './pages/AddItems/AddItems';
 import Header from './pages/Header/Header';
 import Home from './pages/Home/Home';
@@ -8,7 +9,8 @@ import Login from './pages/Login/Login';
 import SignUP from './pages/Login/SignUp/SignUP';
 import ManageItems from './pages/ManageItems/ManageItems';
 import MyItems from './pages/MyItems/MyItems';
-
+import Blog from './pages/Blog/Blog'
+import NotFound from './pages/NotFound/NotFound';
 function App() {
   return (
     <div >
@@ -16,12 +18,14 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUP></SignUP>}></Route>
         <Route path='/manageItems' element={<ManageItems></ManageItems>}></Route>
         <Route path='/addItems' element={<AddItems></AddItems>}></Route>
         <Route path='/myItems' element={<MyItems></MyItems>}></Route>
-        
+        <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
     </div>
